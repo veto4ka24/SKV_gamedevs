3e08ea4046d621469f3cee40cf5601f5c92c5faf
veto4ka24 <zhbanova.es@phystech.edu>
2022-12-18 12:27:26 +0300

    косметические изменения и поле для вопроса

--- cosmetics.py
+++ cosmetics.py
@@ -1,9 +1,9 @@
 import pygame
 
-GREEN = (10, 200, 16)
-DARKGREEN = (0, 130, 50)
-RED = (230, 0, 30)
-DARKRED = (130, 10, 40)
+GREEN = (109, 234, 117)
+DARKGREEN = (40, 96, 51)
+RED = (221, 68, 35)
+DARKRED = (132, 24, 43)
 WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
 
@@ -18,7 +18,7 @@ class Jeenie(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
 
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH*3 / 10, HEIGHT*4 / 13)
+        self.rect.center = (WIDTH*3 / 13, HEIGHT*4 / 13)
 
 
 pygame.init()
@@ -28,7 +28,7 @@ all_sprites.add(akinator)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Угадаю, какой регион России ты загадал!')
 pygame.display.set_icon(pygame.image.load('russianflag.bmp'))
-screen.fill((210, 190, 10))
+screen.fill((255, 254, 112))
 
 pygame.draw.rect(screen, GREEN, (50, 400, 250, 150))
 pygame.draw.rect(screen, DARKGREEN, (50, 400, 250, 150), 8)
@@ -42,10 +42,13 @@ screen.blit(text1, (135, 455))
 text2 = comicsans_font1.render("Нет", True, WHITE)
 screen.blit(text2, (585, 455))
 comicsans_font2 = pygame.font.SysFont("Comic Sans", 25)
+comicsans_font3 = pygame.font.SysFont("Comic Sans", 20)
 rules_l1 = comicsans_font2.render("Правила игры", True, (130, 0, 140))
 rules_l2 = comicsans_font2.render("Вы загадываете какой-то субъект России,", True, (130, 0, 140))
 rules_l3 = comicsans_font2.render(" а Джинн с помощью ваших ответов его угадывает.", True, (130, 0, 140))
 rules_l4 = comicsans_font2.render("Все просто, нужно лишь немного вспомнить географию!", True, (130, 0, 140))
+rules_l5 = comicsans_font3.render('Для ответов на вопросы используйте клавиши "ВПРАВО" и "ВЛЕВО"', True, (140, 0, 100))
+rules_l6 = comicsans_font3.render('Для всплывания окна с вопросом используйте клавишу Q', True, (150, 0, 90))
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -58,11 +61,15 @@ while flagRunning:
             flagRunning = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_h:
-                pygame.draw.rect(screen, WHITE, (30, 330, 740, 220))
+                pygame.draw.rect(screen, WHITE, (30, 330, 740, 260))
                 screen.blit(rules_l1, (290, 360))
                 screen.blit(rules_l2, (120, 400))
                 screen.blit(rules_l3, (70, 430))
                 screen.blit(rules_l4, (70, 460))
+                screen.blit(rules_l5, (70, 510))
+                screen.blit(rules_l6, (95, 545))
+            elif event.key == pygame.K_q:
+                pygame.draw.rect(screen, WHITE, (340, 50, 400, 200))
             elif event.key == pygame.K_LEFT:
                 pygame.draw.rect(screen, (10, 110, 50), (50, 400, 250, 150))
                 #pygame.draw.rect(screen, DARKGREEN, (50, 400, 250, 150), 8)
@@ -72,7 +79,7 @@ while flagRunning:
                 pygame.display.flip()
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_h:
-                screen.fill((210, 190, 10))
+                screen.fill((255, 254, 112))
                 pygame.draw.rect(screen, GREEN, (50, 400, 250, 150))
                 pygame.draw.rect(screen, DARKGREEN, (50, 400, 250, 150), 8)
                 screen.blit(text1, (135, 455))
